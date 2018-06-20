982044da08fb12781094cc41e2d4cf2f6e710ae6
SebHatt <hatt.sebastian@gmail.com>
2018-06-20 09:37:32 +0100

    updated tests

--- Unit and Integration Tasks B/js_calculator_start_point/tests/integration/tests.js
+++ Unit and Integration Tasks B/js_calculator_start_point/tests/integration/tests.js
@@ -82,35 +82,6 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('-0.25');
   })
 
-// //updated below to show 'undefined' not 'Infinity'
-//   it('should show infinity when dividing (not zero) by zero', function(){
-//     running_total = element(by.css('#running_total'));
-//     element(by.css('#number1')).click();
-//     element(by.css('#operator_divide')).click();
-//     element(by.css('#number0')).click();
-//     element(by.css('#operator_equals')).click();
-//     expect(running_total.getAttribute('value')).to.eventually.equal('Infinity');
-//   })
 
-  it('should show infinity when dividing by zero you should get undefined', function(){
-    running_total = element(by.css('#running_total'));
-    element(by.css('#number1')).click();
-    element(by.css('#operator_divide')).click();
-    element(by.css('#number0')).click();
-    element(by.css('#operator_equals')).click();
-    expect(running_total.getAttribute('value')).to.eventually.equal('undefined');
-  })
-
-
-// //can't deal with numbers bigger than 64 bit numbers due to JS limit
-//   it('should work for large numbers: calculate 2x10^20 + 5', function(){
-//     running_total = element(by.css('#running_total'));
-//     element(by.css('#number2')).click();
-//     for(let i=0; i<20; i++){element(by.css('#number0')).click();}
-//     element(by.css('#operator_add')).click();
-//     element(by.css('#number5')).click();
-//     element(by.css('#operator_equals')).click();
-//     expect(running_total.getAttribute('value')).to.eventually.equal('200000000000000000005');
-//   })
 
 });
